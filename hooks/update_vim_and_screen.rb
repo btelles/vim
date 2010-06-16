b942be29ffe1941c6badae685581a2e52653b20e
Bernie Telles <btelles@gmail.com>
2010-06-15 22:41:02 -0400

    add more samples

--- hooks/update_vim_and_screen.rb
+++ hooks/update_vim_and_screen.rb
@@ -5,7 +5,7 @@ dir = File.dirname(__FILE__)
 #Copy all screen settings/bindings
 dest_screenrc_path = File.join(dir, '../../')
 src_screenrc_path = File.join(dir, '../screen/')
-if FileUtils.cp_r(src_screenrc_path, dest_screenrc_path, :force)
+if FileUtils.cp_r(src_screenrc_path, dest_screenrc_path, :f)
   puts "Copied screen to the home directory successfully."
 else
   puts "Failed to copy screen to the home directory."
@@ -17,7 +17,7 @@ settings_files = %w{.vimrc .irbrc .autotest .bashrc}
 settings_files.each do |file_name|
   dest= File.join(dir, "../../#{file_name}")
   src= File.join(dir, "../#{file_name}")
-  if FileUtils.cp_r(src, dest, :force)
+  if FileUtils.cp_r(src, dest, :f)
     puts "Copied #{file_name} to the home directory successfully."
   else
     puts "Failed to copy #{file_name} to the home directory."
