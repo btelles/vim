3ff8ebf7d33e3f88de025f3a0c550cb78aefe498
Bernie Telles <btelles@gmail.com>
2010-06-15 23:05:45 -0400

    try to fix hooks

--- hooks/update_vim_and_screen.rb
+++ hooks/update_vim_and_screen.rb
@@ -9,6 +9,7 @@ src_screenrc_path = File.join(dir, '../screen/')
 FileUtils.rm_rf(dest_screenrc_path) if File.exist?(dest_screenrc_path)
 
 FileUtils.cp_r(src_screenrc_path, dest_screenrc_path_no_dir)
+puts $?.to_s
 if $? == 0
   puts "Copied screen to the home directory successfully."
 else
