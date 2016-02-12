81f60ee76dfe49495a0ee052ba43a72edac2cf1e
Bernardo Telles <btelles@google.com>
2016-02-11 17:29:05 -0800

    Move install_everything to just bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# This is a bootstrap script for setting up a fresh Ubuntu install.
+# To use it, copy the following into your terminal:
+# curl -o- https://raw.githubusercontent.com/btelles/vim/master/bootstrap.sh | bash
+
+echo -n "Installing dependencies"
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev git
+
+sudo apt-get build-dep vim-gnome
+
+echo "Enter the password for your ssh key:"
+
+read pswd
+
+echo "Generating ssh key..."
+
+ssh-keygen -f ~/.ssh/id_rsa -N "$pswd"
+
+echo "Add the following ssh key to my github account:"
+
+cat ~/.ssh/id_rsa.pub
+
+echo "Re-enter the password for your ssh key:"
+
+ssh-add
+
+echo "Press [Enter] key to continue..."
+
+read
+
+echo "Cloning vim repo..."
+git clone git@github.com:btelles/vim.git ~/.vim
+
+~/.vim/gnome-terminal-colors-solarized/install.sh -s light -p Default
+
+echo "Linking preference files..."
+ln -fs ~/.vim/.bashrc ~/
+ln -fs ~/.vim/.gitconfig ~/
+ln -fs ~/.vim/.tmux.conf ~/
+ln -fs ~/.vim/.vimrc ~/
+ln -fs ~/.vim/.ackrc ~/
+ln -fs ~/.vim/.bash_aliases ~/
+ln -fs ~/.vim/.bash_profile ~/
+ln -fs ~/.vim/.rspec ~/
+ln -fs ~/.vim/.autotest ~/
+
+echo "Installing vim submodules..."
+(cd ~/.vim && git submodule init && git submodule update)
+
+echo "installing VIM"
+(cd ~/.vim/vim &&
+./configure \
+    --enable-perlinterp=dynamic \
+    --enable-pythoninterp=dynamic \
+    --enable-rubyinterp=dynamic \
+    --enable-cscope \
+    --enable-gui=auto \
+    --enable-gtk2-check \
+    --enable-gnome-check \
+    --with-features=huge \
+    --with-x \
+    --with-python-config-dir=/usr/lib/python2.7/config &&
+make -j 4 && sudo make install
+)
+
+echo "Copying Consolas font to local directory."
+mkdir -p ~/.fonts
+cp ~/.vim/Consolas.ttf ~/.fonts
+
+echo "Installing Rbenv..."
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo "Installing PyEnv..."
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
